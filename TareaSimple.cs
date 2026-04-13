88ed355ff73b72883500b515cd79a0758648bd34
Sitcka <snapaspuei@gmail.com>
2026-04-13 11:25:37 +0200

    feat(Herencia): Implementar TareaSimple y TareaPrioritaria

---
+++ TareaSimple.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gestor_Tareas
+{
+    public class TareaSimple : Tarea
+    {
+        public TareaSimple(string titulo,
+            DateTime fechaLimite,
+            PrioridadTarea prioridad,
+            string? descripcion = null)
+            :base(titulo, fechaLimite, prioridad, descripcion)
+        {
+            
+        }
+
+        public override string ObtenerResumen()
+        {
+            return $"[SIMPLE] {Titulo} | {Estado} | Vence: {FechaLimite:dd/MM/yy}";
+        }
+    }
+}
